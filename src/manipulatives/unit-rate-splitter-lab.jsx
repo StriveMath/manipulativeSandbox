9cfa2a3c2fc379238ea5fc1bcbe8b66af5f643ec
Veronica David <veronicadavidjames@gmail.com>
2026-06-20 22:59:39 +0530

    Restore non unit rate manipulatives

---
+++ src/manipulatives/unit-rate-splitter-lab.jsx
@@ -0,0 +1,7 @@
+export default function UnitRateSplitterLab() {
+  return (
+    <div className="flex h-[500px] w-[800px] items-center justify-center bg-slate-50 text-slate-500">
+      <p className="text-lg font-semibold">Ratios - Unit rates will be rebuilt here.</p>
+    </div>
+  )
+}
